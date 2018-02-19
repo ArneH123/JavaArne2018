c23e393d79890184242b92e5a832cacc29141b7d
Arne Halsberghe <arne.halsberghe@live.be>
2018-02-19 15:51:54 +0100

    eerste stappen van domein UC1

---
+++ src/domein/Leerkracht.java
@@ -0,0 +1,24 @@
+package domein;
+
+public class Leerkracht {
+
+	public String Naam;
+	private int Wachtwoord;
+
+	public String getNaam() {
+		return this.Naam;
+	}
+
+	public void setNaam(String Naam) {
+		this.Naam = Naam;
+	}
+
+	public int getWachtwoord() {
+		return this.Wachtwoord;
+	}
+
+	public void setWachtwoord(int Wachtwoord) {
+		this.Wachtwoord = Wachtwoord;
+	}
+
+}
