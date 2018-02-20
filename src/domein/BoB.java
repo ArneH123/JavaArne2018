5f94915ade1865202e6398c3e23a9da5ea794172
Arne Halsberghe <arne.halsberghe@live.be>
2018-02-20 13:18:20 +0100

    Code BoB schrijven

--- src/domein/BoB.java
+++ src/domein/BoB.java
@@ -1,4 +1,24 @@
 package domein;
 
+import java.util.ArrayList;
+
 public class BoB {
+    
+    private final ArrayList<Actie> acties;
+    private final ArrayList<Opdracht> opdrachten;
+    private final ArrayList<Toegangscode> toegangscodes;
+    
+    public BoB(ArrayList<Actie> a, ArrayList<Opdracht> o, ArrayList<Toegangscode> t){
+        if(a.size() == 8 && o.size() == 8 && t.size() == 8){
+            acties = a;
+            opdrachten = o;
+            toegangscodes = t;
+        }else{
+            throw new IllegalArgumentException("Elke List moet size 8 zijn!");
+        }
+    }
+    
+    public ArrayList<Actie> GeefActies(){
+        return acties;
+    }
 }
