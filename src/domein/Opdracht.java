21f4564e4eef6e8efea88cc95e276d419ff89ee0
Pieter Stevens <pieter.stevens.u12345@student.hogent.be>
2018-02-21 19:38:52 +0100

    Klasse opdracht

--- src/domein/Opdracht.java
+++ src/domein/Opdracht.java
@@ -1,12 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package domein;
 
-/**
- *
- * @author Arne
- */
-public class Opdracht {}
+public class Opdracht {
+
+    Oefening oefening;
+    Actie actie;
+    Toegangscode toegangscode;
+    final int nummerOpdracht;
+
+    public Opdracht(Oefening oefening, Actie actie, Toegangscode toegangscode, int nummerOpdracht)
+    {
+        this.oefening = oefening;
+        this.actie = actie;
+        this.toegangscode = toegangscode;
+        this.nummerOpdracht = nummerOpdracht;
+    }
+
+    public Oefening getOefening()
+    {
+        return oefening;
+    }
+
+    public void setOefening(Oefening oefening)
+    {
+        this.oefening = oefening;
+    }
+
+    public Actie getActie()
+    {
+        return actie;
+    }
+
+    public void setActie(Actie actie)
+    {
+        this.actie = actie;
+    }
+
+    public Toegangscode getToegangscode()
+    {
+        return toegangscode;
+    }
+
+    public void setToegangscode(Toegangscode toegangscode)
+    {
+        this.toegangscode = toegangscode;
+    }
+
+    public int getNummerOpdracht()
+    {
+        return nummerOpdracht;
+    }
+    
+    
+}
