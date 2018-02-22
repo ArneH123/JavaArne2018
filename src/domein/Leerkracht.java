233267835480ecdc44e7c1791b34bb525fb1b67c
Arne Halsberghe <arne.halsberghe@live.be>
2018-02-22 13:40:00 +0100

    DomeinLogica UC2

--- src/domein/Leerkracht.java
+++ src/domein/Leerkracht.java
@@ -1,9 +1,20 @@
 package domein;
 
+import java.util.ArrayList;
+
 public class Leerkracht {
 
 	public String Naam;
 	private int Wachtwoord;
+        private ArrayList<Oefening> oefeningen;
+
+    public Leerkracht(String Naam, int Wachtwoord) {
+        this.Naam = Naam;
+        this.Wachtwoord = Wachtwoord;
+        oefeningen = new ArrayList<Oefening>();
+    }
+        
+        
 
 	public String getNaam() {
 		return this.Naam;
@@ -20,5 +31,25 @@ public class Leerkracht {
 	public void setWachtwoord(int Wachtwoord) {
 		this.Wachtwoord = Wachtwoord;
 	}
+        
+        public void voegOefeningToe(Oefening oefening){
+            oefeningen.add(oefening);
+        }
+        
+        public void wijzigOpgaveOefening(int Id, String opgave){
+            oefeningen.get(Id).setOpgave(opgave);
+        }
+        
+        public void wijzigAntwoord(int Id, String antwoord){
+            oefeningen.get(Id).setAntwoord(antwoord);
+        }
+        
+        public void wijzigFeedback(int Id, Boolean feedback){
+            oefeningen.get(Id).setFeedback(feedback);
+        }
+        
+//        public void verwijderOefening(Oefening oefening){
+//            oefeningen.remove(oefening.)
+//        }
 
 }
--- src/domein/Oefening.java
+++ src/domein/Oefening.java
@@ -1,4 +1,57 @@
 package domein;
 
+import java.util.ArrayList;
+import javax.persistence.Id;
+
 public class Oefening {
+    @Id
+    private int Id;
+    private String opgave;
+    private double antwoord;
+    private Boolean feedback;
+    private ArrayList<Groepsbewerkingen> groepsbewerkingen;
+
+    public Oefening(String opgave, double antwoord, ArrayList<Groepsbewerkingen> groepsbewerkingen) {
+        this.opgave = opgave;
+        this.antwoord = antwoord;
+        this.groepsbewerkingen = groepsbewerkingen;
+    }
+
+    public String getOpgave() {
+        return opgave;
+    }
+
+    public double getAntwoord() {
+        return antwoord;
+    }
+
+    public Boolean getFeedback() {
+        return feedback;
+    }
+
+    public ArrayList<Groepsbewerkingen> getGroepsbewerkingen() {
+        return groepsbewerkingen;
+    }
+
+    public void setOpgave(String opgave) {
+        this.opgave = opgave;
+    }
+
+    public void setAntwoord(double antwoord) {
+        this.antwoord = antwoord;
+    }
+
+    public void setFeedback(Boolean feedback) {
+        this.feedback = feedback;
+    }
+
+    public void setGroepsbewerkingen(ArrayList<Groepsbewerkingen> groepsbewerkingen) {
+        this.groepsbewerkingen = groepsbewerkingen;
+    }
+
+    void setAntwoord(String antwoord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 }
