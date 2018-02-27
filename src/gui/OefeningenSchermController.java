46c9bf4f07f674f25e0c13c2d711f52ca3710271
ruben <ruben@DESKTOP-T2D40H6>
2018-02-27 22:17:14 +0100

    oefeningschermen verder uitgewerkt

--- src/gui/OefeningenSchermController.java
+++ src/gui/OefeningenSchermController.java
@@ -5,9 +5,14 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -16,9 +21,38 @@ import javafx.fxml.Initializable;
  */
 public class OefeningenSchermController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    public OefeningenSchermController() {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("HoofdMenu.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        
+
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    
+
+    @FXML
+    private Button btnToevoegen;
+    @FXML
+    private Button btnAanpassen;
+    
+    @FXML
+    private void oefeningToevoegen (ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void oefeningAanpassen (ActionEvent event) {
+        
+    }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
