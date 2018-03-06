161af7edbcc6020cc36ec6c2372d46d908878bba
Pieter Stevens <pieter.stevens.u12345@student.hogent.be>
2018-03-06 20:24:50 +0100

    javafx oefeningenScherm

--- src/gui/OefeningenSchermController.java
+++ src/gui/OefeningenSchermController.java
@@ -8,7 +8,9 @@ package gui;
 import domein.Oefening;
 import domein.OefeningBeheerder;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,6 +18,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -28,10 +31,23 @@ public class OefeningenSchermController extends AnchorPane {
     @FXML
     private AnchorPane AnchorPane;
     
-    @FXML
     ListView<String> oefeningenView;
     
     private OefeningBeheerder ob;
+    @FXML
+    private TextField txtZoekOefeningen;
+    @FXML
+    private ListView<?> lstOefeningen;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtOpgave;
+    @FXML
+    private TextField txtFeedback;
+    @FXML
+    private TextField txtAntwoord;
+    @FXML
+    private ListView<?> lstGpb;
 
     public OefeningenSchermController(OefeningBeheerder ob) {
 
@@ -58,11 +74,15 @@ public class OefeningenSchermController extends AnchorPane {
 
     private void buildGui() {
         ObservableList<Oefening> data = ob.geefOefeningenAsLijst();
-        ObservableList<String> names = FXCollections.observableArrayList();
+        ArrayList<String> names = new ArrayList<String>(); //FXCollections.observableArrayList();
         for(Oefening o : data){
             names.add(o.getNaam());
         }
+        
         Collections.sort(names);
-        oefeningenView.setItems(names);
+        lstOefeningen.setItems(
+                    FXCollections.observableArrayList(names)
+                );
+        //oefeningenView.setItems(names);
     }
 }
